a9fa8ff204ce13cafd7945971d2e8927e79fc4ba
CodingMirec <m.sykora01@gmail.com>
2020-12-16 23:56:41 +0100

    work done

--- onas.php
+++ onas.php
@@ -1,13 +1,58 @@
 <?php $title = 'O nás'; ?>
 <?php include "inc/header.php"; ?>
 <?php include "inc/nav.php"; ?>
+<link rel="stylesheet" href="css/style.css?v=<?= time(); ?>" />
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
+<header class="about bg-primary text-center pb-3 mb-5">
+    <div class="container">
+        <h1 class="font-weight-light text-white">Meet the Team</h1>
+    </div>
+</header>
 <div class="container">
-    ONAS
+    <div class="row">
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+                <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-0">Team Member</h5>
+                    <div class="card-text text-black-50">Web Developer</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+                <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-0">Team Member</h5>
+                    <div class="card-text text-black-50">Web Developer</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+                <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-0">Team Member</h5>
+                    <div class="card-text text-black-50">Web Developer</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-0 shadow">
+                <img src="https://source.unsplash.com/ZI6p3i9SbVU/500x350" class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                    <h5 class="card-title mb-0">Team Member</h5>
+                    <div class="card-text text-black-50">Web Developer</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.row -->
+
 </div>
 
 
