541f87c612d3a6c327c88b1504f92cd2a4929ee4
CodingMirec <m.sykora01@gmail.com>
2020-12-17 10:33:03 +0100

    styling changes

--- onas.php
+++ onas.php
@@ -14,7 +14,7 @@
 </header>
 <div class="container">
     <div class="row">
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-0 shadow">
                 <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" class="card-img-top" alt="...">
                 <div class="card-body text-center">
@@ -23,7 +23,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-0 shadow">
                 <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
                 <div class="card-body text-center">
@@ -32,7 +32,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-0 shadow">
                 <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
                 <div class="card-body text-center">
@@ -41,7 +41,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6 mb-4">
+        <div class="col-xl-6 col-md-6 mb-4">
             <div class="card border-0 shadow">
                 <img src="https://source.unsplash.com/ZI6p3i9SbVU/500x350" class="card-img-top" alt="...">
                 <div class="card-body text-center">
