d93153ac4681b6400fb0070e5097c4dc206b437f
CodingMirec <m.sykora01@gmail.com>
2021-01-19 19:25:53 +0100

    added contact page

--- inc/nav.php
+++ inc/nav.php
@@ -12,6 +12,9 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="onas.php">About Us</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="contact.php">Contact</a>
+        </li>
       </ul>
     </div>
   </div>
